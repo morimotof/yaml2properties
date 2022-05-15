e3379afcb7b8f191e79d94bc5361a41ee33a137a
totemoyoiko <morimotof@gmail.com>
2022-05-15 15:19:08 +0900

    モック内でassertすると、assertのエラーもテスト対象のプログラムのエラーハンドラで処理されてしまう。 testプログラムに制御が戻ってからassertするようにした。

--- test_yaml2properties.py
+++ test_yaml2properties.py
@@ -5,6 +5,15 @@ import datetime
 
 class TestYaml2properties(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.outmap = {}
+        cls.outlist = []
+
+    def setUp(self):
+        TestYaml2properties.outmap = {}
+        TestYaml2properties.outlist = []
+
     # 引数の処理を検証する
     def test_parse_args(self):
         args = yaml2properties.Yaml2Properties.parse_args(['-f','-i','sample.yml'])
@@ -15,6 +24,11 @@ class TestYaml2properties(unittest.TestCase):
         assert args.fileName == ['sample.yml']
 
     def _mock_print_line(self, path, target):
+        TestYaml2properties.outmap[path]=target
+    
+    # sample.ymlを変換した結果を検証する
+    @mock.patch("yaml2properties.Yaml2Properties.print_line", new=_mock_print_line)
+    def test_yaml2propertie_print_line(self):
         map = {
              '.simple.string':'sample'
             ,'.simple.kanji':'漢字を表示する'
@@ -33,14 +47,18 @@ class TestYaml2properties(unittest.TestCase):
             ,'.complex[2].name':'test3'
             ,'.complex[2].age':30
         }
-        assert map.get(path) == target
-    
-    # sample.ymlを変換した結果を検証する
-    @mock.patch("yaml2properties.Yaml2Properties.print_line", new=_mock_print_line)
-    def test_yaml2propertie_print_line(self):
         yaml2properties.Yaml2Properties.main(['-f','sample.yml'])
+        for k in map:
+            v1 = map[k]
+            v2 = TestYaml2properties.outmap.get(k,"")
+            assert v1 == v2, 'check key:' + k
 
     def _mock_printout(self, msg):
+        TestYaml2properties.outlist.append(msg)
+
+    # sample.ymlを変換した結果を検証する
+    @mock.patch("yaml2properties.Yaml2Properties.printout", new=_mock_printout)
+    def test_yaml2propertie_printout(self):
         list = [
              'sample.yml:simple.string=sample'
             ,'sample.yml:simple.kanji=漢字を表示する'
@@ -59,21 +77,12 @@ class TestYaml2properties(unittest.TestCase):
             ,'sample.yml:complex[2].name=test3'
             ,'sample.yml:complex[2].age=30'
         ]
-        assert msg in list
-
-    # sample.ymlを変換した結果を検証する
-    @mock.patch("yaml2properties.Yaml2Properties.printout", new=_mock_printout)
-    def test_yaml2propertie_printout(self):
         yaml2properties.Yaml2Properties.main(['sample.yml'])
+        for k in list:
+            assert k in TestYaml2properties.outlist, 'check key:' + k
 
     def _mock_printout2(self, msg):
-        list = [
-             'a.caption=inner class caption'
-            ,'a.date=2022-05-15'
-            ,'b[]=1'
-            ,'c=string'
-        ]
-        assert msg in list
+        TestYaml2properties.outlist.append(msg)
 
     class Inner:
         def __init__(self):
@@ -82,8 +91,16 @@ class TestYaml2properties(unittest.TestCase):
 
     @mock.patch("yaml2properties.Yaml2Properties.printout", new=_mock_printout2)
     def test_yaml2propertie_y2p(self):
+        list = [
+             'a.caption=inner class caption'
+            ,'a.date=2022-05-15'
+            ,'b[]=1'
+            ,'c=string'
+        ]
         args = yaml2properties.Yaml2Properties.parse_args(['-f','-i','test.yml'])
         yaml2properties.Yaml2Properties.of('test.yml',args).y2p('',{'a': self.Inner(),'b':[1],'c':'string'},'.')
+        for k in list:
+            assert k in TestYaml2properties.outlist, 'check key:' + k
 
 if __name__ == "__main__":
     unittest.main()
